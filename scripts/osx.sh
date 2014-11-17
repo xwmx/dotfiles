fe4575069cdd636d615e2e9eb03c63f89c8d730f
William Melody <hi@williammelody.com>
2014-11-17 14:32:32 -0800

    Adding more defaults to osx.sh

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -20,10 +20,45 @@ defaults write com.apple.DiskUtility advanced-image-options -bool true
 defaults write com.apple.finder QLEnableTextSelection -bool true
 
 ###############################################################################
+# Address Book, Dashboard, iCal, TextEdit
+###############################################################################
+
+# Enable the debug menu in Address Book.
+# Revert command:
+# defaults delete com.apple.addressbook ABShowDebugMenu
+defaults write com.apple.addressbook ABShowDebugMenu -bool true
+
+# Enable Dashboard dev mode (allows keeping widgets on the desktop)
+defaults write com.apple.dashboard devmode -bool true
+
+# Use plain text mode for new TextEdit documents
+defaults write com.apple.TextEdit RichText -int 0
+# Open and save files as UTF-8 in TextEdit
+# Revert commands:
+# defaults delete com.apple.TextEdit PlainTextEncoding
+# defaults delete com.apple.TextEdit PlainTextEncodingForWrite
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+###############################################################################
+# Mac App Store
+###############################################################################
+
+# Enable the WebKit Developer Tools in the Mac App Store
+# Revert command:
+# defaults delete com.apple.appstore WebKitDeveloperExtras
+defaults write com.apple.appstore WebKitDeveloperExtras -bool true
+
+# Enable Debug Menu in the Mac App Store
+# Revert command:
+# defaults delete com.apple.appstore ShowDebugMenu
+defaults write com.apple.appstore ShowDebugMenu -bool true
+
+###############################################################################
 # Kill affected applications
 ###############################################################################
 
-for app in "Disk Utility" "Finder"; do
+for app in "Address Book" "Disk Utility" "Finder"; do
 	killall "${app}" > /dev/null 2>&1
 done
 echo "Done. Note that some of these changes require a logout/restart to take effect."
