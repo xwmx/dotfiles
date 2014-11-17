682bad049d425a276211b5dadc9fafd7c6ed3d96
William Melody <hi@williammelody.com>
2014-11-17 13:18:18 -0800

    Updating osx.sh to include initial finder config and app reloading

--- scripts/osx.sh
+++ scripts/osx.sh
@@ -1,6 +1,8 @@
+###############################################################################
 # Disk Utility
+###############################################################################
 
-# Enable the debug menu. Reset command:
+# Enable the debug menu. Revert command:
 # $ defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool false
 defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 
@@ -8,3 +10,20 @@ defaults write com.apple.DiskUtility DUDebugMenuEnabled -bool true
 # TODO: this was already set to `"advanced-image-options" = 1`, so the actual
 # default will need to be determined.
 defaults write com.apple.DiskUtility advanced-image-options -bool true
+
+###############################################################################
+# Finder
+###############################################################################
+
+# Finder: allow text selection in Quick Look. Revert command:
+# $ defaults delete com.apple.finder QLEnableTextSelection
+defaults write com.apple.finder QLEnableTextSelection -bool true
+
+###############################################################################
+# Kill affected applications
+###############################################################################
+
+for app in "Disk Utility" "Finder"; do
+	killall "${app}" > /dev/null 2>&1
+done
+echo "Done. Note that some of these changes require a logout/restart to take effect."
