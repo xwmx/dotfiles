9ffc2be3a51318438d22ddde9b865819fae9c8f0
William Melody <hi@williammelody.com>
2014-11-27 18:30:01 -0800

    Add script/npm.sh
    npm.sh provides a convenient way to install various global node.js modules

---
+++ script/npm.sh
@@ -0,0 +1,18 @@
+# npm.sh
+#
+# Install various global node modules
+
+# http://bower.io/
+npm install -g bower
+
+# http://coffeescript.org/
+npm install -g coffee-script
+
+# http://jashkenas.github.io/docco/
+npm install -g docco
+
+# http://gruntjs.com/
+npm install -g grunt-cli
+
+# http://yeoman.io/
+npm install -g yo
