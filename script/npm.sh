bfa65f422377c1eb96329297e9cd2cff96a0954a
William Melody <hi@williammelody.com>
2014-11-27 18:41:37 -0800

    Adding slap to npm install list

--- script/npm.sh
+++ script/npm.sh
@@ -14,5 +14,9 @@ npm install -g docco
 # http://gruntjs.com/
 npm install -g grunt-cli
 
+# https://github.com/slap-editor/slap
+# slap is a Sublime-like terminal-based text editor that strives to make editing from the terminal easier
+npm install -g slap
+
 # http://yeoman.io/
 npm install -g yo
