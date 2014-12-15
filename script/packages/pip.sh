b195e393c787fdb918f88c360a7652d692da6735
William Melody <hi@williammelody.com>
2014-12-14 16:34:27 -0800

    Adding python 3 section in pip.sh

--- script/packages/pip.sh
+++ script/packages/pip.sh
@@ -1,12 +1,26 @@
 #!/usr/bin/env bash
 
-# Install script for pip-installed Python packages
+# Install script for pip-installed Python packages.
+#
+# NOTE: both python 2 and python 3 are included in this script, which assumes
+# both are installed.
 #
 # http://www.pip-installer.org/
 # https://pip.pypa.io
+
+###############################################################################
+# Python 2
+###############################################################################
 
 pip install --upgrade setuptools
 pip install --upgrade pip
 
 # http://ipython.org/
 pip install ipython[all]
+
+###############################################################################
+# Python 3
+###############################################################################
+
+pip3 install --upgrade pip
+
