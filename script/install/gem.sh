64b1ec09f19ec8b830036113bd3fc6e3cead7f1f
William Melody <hi@williammelody.com>
2014-12-20 19:43:07 -0800

    Adding gem.sh install script with ruby-lint as an initial install.

---
+++ script/install/gem.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+#
+# gem.sh
+#
+# Install various global ruby gems modules
+
+# ruby-lint
+#
+# Static code analysis for Ruby.
+#
+# https://github.com/YorickPeterse/ruby-lint
+gem install ruby-lint
