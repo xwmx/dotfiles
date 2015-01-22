a5c9e98ef2303d45cfac2257687113717bdc4b39
William Melody <hi@williammelody.com>
2015-01-22 11:49:48 -0800

    Move git bash settings to separate git.sh file

---
+++ home/.bashrc.d/git.sh
@@ -0,0 +1,19 @@
+#==============================================================================
+# Git
+#==============================================================================
+
+alias gb='git branch'
+alias gba='git branch -a'
+alias gstat='git status'
+alias gc='git commit -v'
+alias gca='git commit -v -a'
+alias gd='git diff | mate'
+alias gl='git pull'
+alias gp='git push'
+alias glog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(magenta)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+
+# Alias the `git` command to the `hub` command.
+# http://github.com/defunkt/hub
+if program_exists hub; then
+  alias git=hub
+fi
