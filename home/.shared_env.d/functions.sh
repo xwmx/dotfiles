1295dee841924219a239f48a9b9ee77783a2affc
William Melody <hi@williammelody.com>
2015-01-24 21:42:31 -0800

    Add weather, define, and epoch functions to shared_env.d
    These are three useful functions found via quora:
    
    http://www.quora.com/What-are-some-of-the-best-command-prompt-or-the-terminal-tricks
    
    Before committing they were slightly modified for clarity and
    consistency with the rest of the files.

--- home/.shared_env.d/functions.sh
+++ home/.shared_env.d/functions.sh
@@ -245,4 +245,62 @@ tre() {
   tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX
 }
 
+# Easily extract all compressed file types
+extract () {
+  if [ -f "$1" ] ; then
+    case $1 in
+      *.tar.bz2)   tar xvjf -- "$1"    ;;
+      *.tar.gz)    tar xvzf -- "$1"    ;;
+      *.bz2)       bunzip2 -- "$1"     ;;
+      *.rar)       unrar x -- "$1"     ;;
+      *.gz)        gunzip -- "$1"      ;;
+      *.tar)       tar xvf -- "$1"     ;;
+      *.tbz2)      tar xvjf -- "$1"    ;;
+      *.tgz)       tar xvzf -- "$1"    ;;
+      *.zip)       unzip -- "$1"       ;;
+      *.Z)         uncompress -- "$1"  ;;
+      *.7z)        7z x -- "$1"        ;;
+      *)           printf "don't know how to extract '%s'...\n" "$1" ;;
+    esac
+  else
+    printf "'%s' is not a valid file" "$1"
+  fi
+}
 
+# Get weather data via the Wunderground API
+#
+# Usage:
+#   weather         # weather for 93015 (Santa Barbara)
+#   weather [query] # weather data for a given query, zip codes preferred.
+weather() {
+  curl -s \
+"http://api.wunderground.com/auto/wui/geo/ForecastXML/index.xml?query=${@:-93105}" | \
+perl -ne 's/&amp;deg;/°/g;/<title>([^<]+)/&&printf "%s: ",$1;/<fcttext>([^<]+)/&&print $1,"\n"'
+}
+
+# Define a word using collinsdictionary.com
+#
+# Usage:
+#  define [word]
+define() {
+  curl -s "http://www.collinsdictionary.com/dictionary/english/$*" | \
+    sed -n '/class="def"/p' | \
+    awk '{gsub(/.*<span class="def">|<\/span>.*/,"");print}' | \
+    sed "s/<[^>]\+>//g"
+}
+
+# Epoch time conversion
+epoch() {
+  TESTREG="[\d{10}]"
+  if [[ "$1" =~ $TESTREG ]]; then
+    # is epoch
+    date -d @$*
+  else
+    # is date
+    if [ $# -gt 0 ]; then
+      date +%s --date="$*"
+    else
+      date +%s
+    fi
+  fi
+}
