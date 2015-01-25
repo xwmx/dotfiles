87d83907905b1372980bf664dc039d39bcd0abb0
William Melody <hi@williammelody.com>
2015-01-25 07:58:31 -0800

    Move `extract` to separate script in bin

--- home/.shared_env.d/functions.sh
+++ home/.shared_env.d/functions.sh
@@ -245,29 +245,6 @@ tre() {
   tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX
 }
 
-# Easily extract all compressed file types
-extract () {
-  if [ -f "$1" ] ; then
-    case $1 in
-      *.tar.bz2)   tar xvjf -- "$1"    ;;
-      *.tar.gz)    tar xvzf -- "$1"    ;;
-      *.bz2)       bunzip2 -- "$1"     ;;
-      *.rar)       unrar x -- "$1"     ;;
-      *.gz)        gunzip -- "$1"      ;;
-      *.tar)       tar xvf -- "$1"     ;;
-      *.tbz2)      tar xvjf -- "$1"    ;;
-      *.tgz)       tar xvzf -- "$1"    ;;
-      *.zip)       unzip -- "$1"       ;;
-      *.Z)         uncompress -- "$1"  ;;
-      *.7z)        7z x -- "$1"        ;;
-      *)           printf "don't know how to extract '%s'...\n" "$1" ;;
-    esac
-  else
-    printf "'%s' is not a valid file" "$1"
-  fi
-}
-
-
 # Epoch time conversion
 epoch() {
   TESTREG="[\d{10}]"
