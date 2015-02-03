57ed8e9a54426e4307de210170a975e3654cce86
William Melody <hi@williammelody.com>
2015-02-03 09:33:25 -0800

    Use base16 twilight theme for shell and include favorites in profile.d

--- home/.profile.d/base16-shell.sh
+++ home/.profile.d/base16-shell.sh
@@ -10,5 +10,20 @@
 # https://github.com/chriskempson/base16-shell
 #==============================================================================
 
-BASE16_SHELL="$HOME/.config/base16-shell/base16-default.dark.sh"
+# Including favorites here for easier switching.
+
+#_base_16_shell_theme="3024"
+#_base_16_shell_theme="bespin"
+#_base_16_shell_theme="brewer"
+#_base_16_shell_theme="eighties"
+#_base_16_shell_theme="google"
+#_base_16_shell_theme="grayscale"
+#_base_16_shell_theme="greenscreen"
+#_base_16_shell_theme="isotope"        # close to original
+#_base_16_shell_theme="shapeshifter"   # close to original
+#_base_16_shell_theme="summerfruit"
+#_base_16_shell_theme="tomorrow"
+_base_16_shell_theme="twilight"
+
+BASE16_SHELL="$HOME/.config/base16-shell/base16-$_base_16_shell_theme.dark.sh"
 [[ -s "$BASE16_SHELL" ]] && source "$BASE16_SHELL"
