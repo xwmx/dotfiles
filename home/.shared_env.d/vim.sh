6360a58a13f2dbff22110db0b781336c97c5b47e
William Melody <hi@williammelody.com>
2015-03-02 16:18:19 -0800

    Set $VIMRUNTIME to MacVim.app when present

---
+++ home/.shared_env.d/vim.sh
@@ -0,0 +1,20 @@
+###############################################################################
+# vim
+#
+# vim environment settings
+#
+# http://www.vim.org/
+# https://code.google.com/p/macvim/
+# http://neovim.org/
+###############################################################################
+
+# If MacVim is installed, then set VIMRUNTIME to MacVim's runtime path. The
+# primary reason for this in the current configuration is to make Neovim
+# (nvim) perform much better on OS X.
+#
+# https://github.com/neovim/neovim/issues/193
+# http://vim.wikia.com/wiki/Set_VIMRUNTIME_within_vimrc
+if program_exists "mvim"; then
+  _macvim_app_path="$HOME/Applications/MacVim.app"
+  export VIMRUNTIME="$_macvim_app_path/Contents/Resources/vim/runtime"
+fi
