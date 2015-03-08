d2efd320bdfc0ea9180b3dc4a8757556b50d4fe8
William Melody <hi@williammelody.com>
2015-03-08 15:14:14 -0700

    Add git.sh and unalias gg

---
+++ home/.profile.d/git.sh
@@ -0,0 +1,18 @@
+###############################################################################
+# git.sh
+#
+# Settings for git. Additional settings can be found in the following
+# locations:
+#   - $HOME/.oh-my-zsh/plugins/git/git.plugin.zsh
+#   - $HOME/.gitconfig
+#   - $HOME/.gitconfig.local
+#
+###############################################################################
+
+# gg is aliased to 'git gui citool' at the following location:
+#
+#   $HOME/.oh-my-zsh/plugins/git/git.plugin.zsh#105
+#
+# However, in order to use the gg command line tool (installed via
+# node-packages), this alias must be unset.
+unalias gg
