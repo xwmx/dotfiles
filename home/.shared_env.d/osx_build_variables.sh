31ce29e8cd951d06aa3605215448689624249850
William Melody <hi@williammelody.com>
2015-03-19 10:21:33 -0700

    Add opam-packages install script
    The initial target package in Irmin, but this also requires installing
    the cohttp and git packages.
    
    Also, the presence of `export LDFLAGS='-arch x86_64'` in the environment
    results in build failures, so unset this in the opam-packages install
    script and include a not in the osx_build_variables.sh file in case this
    issue comes up in other contexts.

--- home/.shared_env.d/osx_build_variables.sh
+++ home/.shared_env.d/osx_build_variables.sh
@@ -5,5 +5,8 @@
 if is_osx; then
   export ARCHFLAGS='-arch x86_64'
   export CFLAGS='-arch x86_64'
+  # NOTE: LDFLAGS set to '-arch x86_64' causes build failures when installing
+  # OPAM packages, and therefore this needs to be unset in
+  # script/install/opam-packages
   export LDFLAGS='-arch x86_64'
 fi
