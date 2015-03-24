57878774751f88ac2422471bdbd79168f1a50328
William Melody <hi@williammelody.com>
2015-03-23 17:10:43 -0700

    Add OPAM and OCaml environment variables.

---
+++ home/.shared_env.d/opam.sh
@@ -0,0 +1,25 @@
+###############################################################################
+# OPAM / OCaml env configuration
+#
+# The settings here are the output of the `opam config env` command, with some
+# slight edits, specifically splitting them up to invidisual lines and using
+# `$HOME` for the home path segment rather than the hard-coded home directory.
+###############################################################################
+
+CAML_LD_LIBRARY_PATH="$HOME/.opam/system/lib/stublibs:/usr/local/lib/ocaml/stublibs"
+export CAML_LD_LIBRARY_PATH
+
+OPAMUTF8MSGS="1"
+export OPAMUTF8MSGS
+
+MANPATH=":$HOME/.opam/system/man"
+export MANPATH
+
+PERL5LIB="$HOME/.opam/system/lib/perl5:"
+export PERL5LIB
+
+OCAML_TOPLEVEL_PATH="$HOME/.opam/system/lib/toplevel"
+export OCAML_TOPLEVEL_PATH
+
+PATH="$HOME/.opam/system/bin:$PATH"
+export PATH
