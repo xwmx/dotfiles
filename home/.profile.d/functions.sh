6e3e09049f1c895fb0dc0679c9ded3599a445c82
William Melody <hi@williammelody.com>
2015-04-13 22:12:40 -0700

    Refactor `gz` shell function and expand description

--- home/.profile.d/functions.sh
+++ home/.profile.d/functions.sh
@@ -11,11 +11,20 @@ chromekill() {
 
 # Archives / Compression ------------------------------------------------------
 
-# Compare original and gzipped file size
+# gz()
+#
+# Usage:
+#   gz [<file path>]
+#
+# Description:
+#   Compare original and gzipped file size.
 gz() {
-  local origsize=$(wc -c < "$1")
-  local gzipsize=$(gzip -c "$1" | wc -c)
-  local ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l)
+  local origsize
+  local gzipsize
+  local ratio
+  origsize=$(wc -c < "$1")
+  gzipsize=$(gzip -c "$1" | wc -c)
+  ratio=$(echo "$gzipsize * 100 / $origsize" | bc -l)
   printf "orig: %d bytes\n" "$origsize"
   printf "gzip: %d bytes (%2.2f%%)\n" "$gzipsize" "$ratio"
 }
