5010bdd565c417739085a12949948a759b9552a0
William Melody <hi@williammelody.com>
2015-05-09 16:35:16 -0700

    Add short `google` and `ddg` aliases for `search` in shell profile.

---
+++ home/.profile.d/search.sh
@@ -0,0 +1,16 @@
+###############################################################################
+# Search Aliases
+#
+# Aliases for the `search` command, found at ~/bin/search
+###############################################################################
+
+__search_cmd="$(which search)"
+if [[ -n "${__search_cmd}" ]]; then
+  google() {
+    "${__search_cmd}" "google" "${@}"
+  }
+
+  ddg() {
+    "${__search_cmd}" "ddg" "${@}"
+  }
+fi
