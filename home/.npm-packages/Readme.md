d366baf54e7a22826f452d1134481dbff73b0633
William Melody <hi@williammelody.com>
2015-05-29 17:15:28 -0700

    Reconfigure npm to install in $HOME/.npm-packages
    This includes renaming the node-packages install script to npm-packages
    for consistency.
    
    Restructuring to install locally ensures that npm packages won't be
    mixed together with other programs in /usr/local (the previous prefix)
    and ensures that the npm configuration is local to the user.

---
+++ home/.npm-packages/Readme.md
@@ -0,0 +1,15 @@
+# .npm-packages
+
+Install location for globally install npm packages.
+
+- https://www.npmjs.com/
+- https://github.com/npm/npm
+
+Set with .npmrc
+
+- [home/.npmrc](../.npmrc)
+- https://docs.npmjs.com/files/npmrc
+
+
+npm-packages install script:
+- [script/install/npm-packages](../../script/install/npm-packages)
---
+++ home/.shared_env.d/npm.sh
@@ -0,0 +1,11 @@
+###############################################################################
+# npm
+#
+# Package manager for JavaScript
+#
+# https://www.npmjs.com/
+###############################################################################
+
+# .npmrc sets the install prefix to ~/.npm-packages
+PATH="$PATH:$HOME/.npm-packages/bin"
+export PATH
