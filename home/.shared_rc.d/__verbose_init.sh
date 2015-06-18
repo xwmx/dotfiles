5b6302b78692e0ba346df2c27316931fd58e8486
William Melody <hi@williammelody.com>
2015-06-18 14:43:17 -0700

    Add __verbose_init.sh files to startup directories.
    When performing a verbose shell startup, these files print their current
    paths, confirming that the files in these directories are being sourced.

---
+++ home/.shared_rc.d/__verbose_init.sh
@@ -0,0 +1,11 @@
+###############################################################################
+# Verbose Init
+#
+# Print the current file path when starting up with verbose output. This file's
+# presence makes it possible to confirm that files in this directory are
+# sourced.
+###############################################################################
+
+. "$HOME/.shared_verbose_init"
+__verbose_init printf "Loading %s\n" \
+  "$([[ -n $BASH_VERSION ]] && echo "$BASH_SOURCE" || echo "$0")"
