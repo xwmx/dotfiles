879c620772cd5aa7ed8508b75a045ee6629542c8
William Melody <hi@williammelody.com>
2015-06-25 13:52:52 -0700

    Add scala formula caveat note to .shared_env.d/scala.sh

--- home/.shared_env.d/scala.sh
+++ home/.shared_env.d/scala.sh
@@ -5,5 +5,9 @@
 ###############################################################################
 
 # Homebrew-installed Scala
+#
+# From scala formula caveats:
+#   To use with IntelliJ, set the Scala home to:
+#   /usr/local/opt/scala/idea
 export SCALA_HOME="/usr/local/opt/scala"
 export PATH=$PATH:$SCALA_HOME/bin
