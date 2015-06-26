7a86b1964bf6a1d32b7b4048e373e5d2ca50c8f3
William Melody <hi@williammelody.com>
2015-06-25 21:34:48 -0700

    Add iterm2 shell integration for zsh and bash.

---
+++ home/.bashrc.d/iterm2.bash
@@ -0,0 +1,20 @@
+###############################################################################
+# iTerm2 Shell Integration
+#
+# iTerm2 may be integrated with the unix shell so that it can keep track of
+# your command history, current working directory, host name, and more--even
+# over ssh. This enables several useful features.
+#
+# Usage:
+#   https://iterm2.com/shell_integration.html
+#
+# https://www.iterm2.com/
+# https://github.com/gnachman/iTerm2
+###############################################################################
+
+# bash integration startup file
+#
+# Received via command:
+#   curl -L https://iterm2.com/misc/bash_startup.in >> \
+#     ~/.iterm2_shell_integration.bash
+source "$HOME/.iterm2_shell_integration.bash"
---
+++ home/.zshrc.d/iterm2.zsh
@@ -0,0 +1,20 @@
+###############################################################################
+# iTerm2 Shell Integration
+#
+# iTerm2 may be integrated with the unix shell so that it can keep track of
+# your command history, current working directory, host name, and more--even
+# over ssh. This enables several useful features.
+#
+# Usage:
+#   https://iterm2.com/shell_integration.html
+#
+# https://www.iterm2.com/
+# https://github.com/gnachman/iTerm2
+###############################################################################
+
+# zsh integration startup file
+#
+# Received via command:
+#   curl -L https://iterm2.com/misc/zsh_startup.in >> \
+#     ~/.iterm2_shell_integration.zsh
+source "$HOME/.iterm2_shell_integration.zsh"
