6e608835f4b1f05f5068974a0d859494ae0b6aee
William Melody <hi@williammelody.com>
2015-07-06 14:42:28 -0700

    Add fasd to brew install list, with initialization in .shared_rc.d

---
+++ home/.shared_rc.d/fasd.sh
@@ -0,0 +1,19 @@
+###############################################################################
+# fasd
+#
+# Command-line productivity booster, offers quick access to files and
+# directories, inspired by autojump, z and v.
+#
+# NOTE: installed via homebrew.
+#
+# https://github.com/clvv/fasd
+###############################################################################
+
+# Initialize all.
+#
+# This will setup a command hook that executes on every command and advanced
+# tab completion for zsh and bash.
+#
+# For more specific initialization options, see:
+# https://github.com/clvv/fasd#install
+eval "$(fasd --init auto)"
