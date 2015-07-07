4825069c8cc8d71b51971ff7028ef2b6fa7aedc3
William Melody <hi@williammelody.com>
2015-07-07 13:35:08 -0700

    Add autoenv to brew install list and .shared_env.d

---
+++ home/.shared_env.d/autoenv.sh
@@ -0,0 +1,20 @@
+###############################################################################
+# Autoenv: Directory-based Environments
+#
+# If a directory contains a `.env` file, it will automatically be executed
+# when you `cd` into it.
+#
+# This is great for...
+#
+# - auto-activating virtualenvs
+# - project-specific environment variables
+# - making millions
+# - Foreman env files are completely compatible.
+#
+#You can also nest envs within eachother.
+#
+# https://github.com/kennethreitz/autoenv
+###############################################################################
+
+# Initialize
+source "/usr/local/opt/autoenv/activate.sh"
