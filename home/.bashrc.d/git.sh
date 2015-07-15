3d11c463ed8c7d7c66dee0bc026c7227b23670ea
William Melody <hi@williammelody.com>
2015-07-14 18:47:49 -0700

    Reformat bash `glog` alias.

--- home/.bashrc.d/git.sh
+++ home/.bashrc.d/git.sh
@@ -10,7 +10,12 @@ alias gca='git commit -v -a'
 alias gd='git diff | mate'
 alias gl='git pull'
 alias gp='git push'
-alias glog="git log --all --graph --pretty=format:'%Cred%h%Creset -%C(magenta)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit --date=relative"
+alias glog="git log \
+  --all \
+  --graph \
+  --pretty=format:'%Cred%h%Creset -%C(magenta)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' \
+  --abbrev-commit \
+  --date=relative"
 
 # Alias the `git` command to the `hub` command.
 # http://github.com/defunkt/hub
