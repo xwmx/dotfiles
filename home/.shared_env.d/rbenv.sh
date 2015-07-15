1ab4e42bc92070d675a4882ffc2d88a2a6c52557
William Melody <hi@williammelody.com>
2015-07-15 15:25:00 -0700

    Remove shell-specific rbenv initialization.
    Shell-specific initialization doesn't appear to be strictly needed, and
    the initialization in this file was duplicated, anyway, causing
    duplicate entries for .rbenv/shims in $PATH.

--- home/.shared_env.d/rbenv.sh
+++ home/.shared_env.d/rbenv.sh
@@ -12,17 +12,3 @@ if which rbenv > /dev/null
 then
   eval "$(rbenv init -)"
 fi
-
-if [ -n "$ZSH_VERSION" ]
-then
-  if which rbenv > /dev/null
-  then
-    eval "$(rbenv init - zsh)"
-  fi
-elif [ -n "$BASH_VERSION" ]
-then
-  if which rbenv > /dev/null
-  then
-    eval "$(rbenv init - bash)"
-  fi
-fi
