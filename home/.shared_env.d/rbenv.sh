2095765922a1e69e7cafa9e18361439e98690d2d
William Melody <hi@williammelody.com>
2015-07-15 15:18:41 -0700

    Use conventional formatting for .shared_env.d/rbenv.sh
    - prefer newlines over semicolons
    - quote variables

--- home/.shared_env.d/rbenv.sh
+++ home/.shared_env.d/rbenv.sh
@@ -8,10 +8,21 @@
 
 export PATH="$HOME/.rbenv/bin:$PATH"
 
-if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+if which rbenv > /dev/null
+then
+  eval "$(rbenv init -)"
+fi
 
-if [ -n $ZSH_VERSION ]; then
-  if which rbenv > /dev/null; then eval "$(rbenv init - zsh)"; fi
-elif [ -n $BASH_VERSION ]; then
-  if which rbenv > /dev/null; then eval "$(rbenv init - bash)"; fi
+if [ -n "$ZSH_VERSION" ]
+then
+  if which rbenv > /dev/null
+  then
+    eval "$(rbenv init - zsh)"
+  fi
+elif [ -n "$BASH_VERSION" ]
+then
+  if which rbenv > /dev/null
+  then
+    eval "$(rbenv init - bash)"
+  fi
 fi
