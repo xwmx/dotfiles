e8a7c60e0020b554de4e32f338bcc88ada462ce0
William Melody <hi@williammelody.com>
2015-07-14 17:25:44 -0700

    Use preferred `if..then` convention in ls.sh

--- home/.bashrc.d/ls.sh
+++ home/.bashrc.d/ls.sh
@@ -3,7 +3,8 @@
 ###############################################################################
 
 # Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
+if ls --color > /dev/null 2>&1
+then # GNU `ls`
   colorflag="--color"
 else # OS X `ls`
   colorflag="-G"
