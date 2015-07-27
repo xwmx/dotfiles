ae6b15e59dbae6214745515777294b8561fba4a9
William Melody <hi@williammelody.com>
2015-07-27 16:05:55 -0700

    Prefer newline rather than semicolon in `.shared_rc.d/ruby.sh`

--- home/.shared_rc.d/ruby.sh
+++ home/.shared_rc.d/ruby.sh
@@ -5,7 +5,8 @@
 # `ruby` on its own launches irb. Have `ruby` call `irb` when there is no
 # argument.
 ruby_or_irb() {
-  if [ -z "$1" ]; then
+  if [ -z "$1" ]
+  then
     irb
   else
     ruby "$@"
