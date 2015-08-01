70fec844e7b88ef499845e49634fa7a76bdb26cb
William Melody <hi@williammelody.com>
2015-08-01 16:01:31 -0700

    Add .shared_rc.d/bask.sh with `b` alias for `bask`

---
+++ home/.shared_rc.d/bask.sh
@@ -0,0 +1,9 @@
+###############################################################################
+# bask - shortcuts
+#
+# A mini-framework for command-centric Bash scripts.
+#
+# https://github.com/alphabetum/bask
+###############################################################################
+
+alias b='bask'
