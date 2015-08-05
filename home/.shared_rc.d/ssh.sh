7432574d324b2dd95612df38a91fc0b59ff17d3e
William Melody <hi@williammelody.com>
2015-08-04 17:37:15 -0700

    Add `generate_ssh_key` shell function.
    Since keys are generated with a known format, this format can also be
    used with `IdentityFile` in .ssh/config.d/*

--- home/.shared_rc.d/ssh.sh
+++ home/.shared_rc.d/ssh.sh
@@ -35,6 +35,43 @@ generate_ssh_config() {
   cat ~/.ssh/default.sshconfig >> ~/.ssh/config
 }
 
+# generate_ssh_key
+#
+# Usage:
+#   generate_ssh_key "user@host.tld"
+#
+# Description:
+#   Generates an ssh key. The generated file uses the following format:
+#     <local.host>_<remote_user>@<remote host>_id_rsa
+#     <local.host>_<remote_user>@<remote.host>_id_rsa
+#
+#   More information: http://askubuntu.com/a/423297
+generate_ssh_key() {
+  [[ -z "${1:-}" ]] && printf "<user@host.tld> required.\n" && return 1
+  local _local_host
+  local _username
+  local _remote_host
+
+  _local_host="$(hostname)"
+  _remote_host="$(echo "${1}" | awk -F '@' '{print $2}')"
+  if [[ -z "${_remote_host:-}" ]]
+  then
+    _remote_host="$(echo "${1}" | awk -F '@' '{print $1}')"
+    ssh-keygen \
+      -t rsa \
+      -b 4096 \
+      -C "${_remote_host}_id_rsa" \
+      -f "${HOME}/.ssh/${_local_host}_${_remote_host}_id_rsa"
+  else
+    _username="$(echo "${1}" | awk -F '@' '{print $1}')"
+    ssh-keygen \
+      -t rsa \
+      -b 4096 \
+      -C "${_username}@${_remote_host}_id_rsa"
+      -f "${HOME}/.ssh/${_local_host}_${_username}@${_remote_host}_id_rsa"
+  fi
+}
+
 # Alias `ssh` to a function that combines the contents of `.ssh/config.d` and
 # `.ssh/config` into a `.ssh/config-all` file that is then used as the config
 # file for `ssh`.
