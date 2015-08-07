7cb3247313808549542c3e2a114ffd62ba5d80ba
William Melody <hi@williammelody.com>
2015-08-07 15:10:33 -0700

    Add bropages to gems install list and alias with `eg`.

---
+++ home/.shared_rc.d/eg.sh
@@ -0,0 +1,13 @@
+###############################################################################
+# bro pages
+#
+# bro pages are a highly readable supplement to man pages.
+#
+# bro pages show concise, common-case examples for Unix commands.
+#
+# http://bropages.org/
+###############################################################################
+
+# Create an `eg` alias for the `bro` cammand, since it's potentially easier
+# to remember.
+alias eg="bro"
