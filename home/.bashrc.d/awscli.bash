b076230b973325a8591fb331d22a9a5d2f62f6e1
William Melody <hi@williammelody.com>
2015-08-07 17:26:58 -0700

    Add awscli to pip3-packages install list and completion to [bash|zsh]_rc.d

---
+++ home/.bashrc.d/awscli.bash
@@ -0,0 +1,17 @@
+###############################################################################
+# AWS Command Line Interface (CLI)
+#
+# The AWS Command Line Interface (CLI) is a unified tool to manage your AWS
+# services. With just one tool to download and configure, you can control
+# multiple AWS services from the command line and automate them through
+# scripts.
+#
+# The AWS CLI introduces a new set of simple file commands for efficient file
+# transfers to and from Amazon S3.
+#
+# https://aws.amazon.com/cli/
+###############################################################################
+
+# Load Bash autocompletion.
+# http://docs.aws.amazon.com/cli/latest/userguide/cli-command-completion.html
+complete -C '/usr/local/bin/aws_completer' aws
