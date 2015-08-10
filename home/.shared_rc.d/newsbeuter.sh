4a6e04a8a58fbd55fec7d23a2de018e14bce74a7
William Melody <hi@williammelody.com>
2015-08-09 20:31:16 -0700

    Configure newsbueter.
    - Add initial example selection of URLs
    - Alias `newsbeuter` with `feed`

---
+++ home/.shared_rc.d/newsbeuter.sh
@@ -0,0 +1,14 @@
+###############################################################################
+# Newsbeuter
+#
+# Newsbeuter is an open-source RSS/Atom feed reader for text terminals. It
+# runs on Linux, FreeBSD, Mac OS X and other Unix-like operating systems.
+# Newsbeuter’s great configurability and vast number of features make it a
+# perfect choice for people that need a slick and fast feed reader that can
+# be completely controlled via keyboard.
+#
+# http://www.newsbeuter.org/
+# https://github.com/akrennmair/newsbeuter/
+###############################################################################
+
+alias feeds="newsbeuter"
