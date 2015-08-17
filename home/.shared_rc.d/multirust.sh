b8d1d3ef08fad28a958ccec6b7a8850c89ffeea7
William Melody <hi@williammelody.com>
2015-08-17 16:23:25 -0700

    Add `mr` alias for `multirust`

---
+++ home/.shared_rc.d/multirust.sh
@@ -0,0 +1,12 @@
+###############################################################################
+# multirust
+#
+# A simple tool for managing multiple installations of the Rust toolchain. It
+# replaces the standard Rust toolchain with components that dynamically choose
+# between alternate implementations based on context.
+#
+# https://github.com/brson/multirust
+###############################################################################
+
+# `multirust` is long, so create an `mr` alias.
+alias mr="multirust"
