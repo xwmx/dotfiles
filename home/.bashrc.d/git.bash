46c77119f8b57fd51ff4972501347749b19f2b4d
William Melody <hi@williammelody.com>
2015-08-25 17:02:43 -0700

    Use current style conventions for git.bash

--- home/.bashrc.d/git.bash
+++ home/.bashrc.d/git.bash
@@ -19,6 +19,7 @@ alias glog="git log \
 
 # Alias the `git` command to the `hub` command.
 # http://github.com/defunkt/hub
-if program_exists hub; then
-  alias git=hub
+if program_exists "hub"
+then
+  alias git="hub"
 fi
