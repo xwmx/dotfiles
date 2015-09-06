cb4d09a59716b2fd9d3091857f20948850b62654
William Melody <hi@williammelody.com>
2015-09-05 18:12:31 -0700

    Add comments about saving bash history in dated files.

--- home/.bashrc.d/history.bash
+++ home/.bashrc.d/history.bash
@@ -10,3 +10,7 @@ export HISTTIMEFORMAT="[%F %T] "
 # Lots o' history.
 export HISTSIZE=10000
 export HISTFILESIZE=10000
+# Save history in dated files.
+# via: https://twitter.com/michaelhoffman/status/639178145673932800
+# NOTE: Not currently in use, but saved here for reference.
+# HISTFILE="${HOME}/.history/$(date -u +%Y/%m/%d.%H.%M.%S)_${HOSTNAME_SHORT}_$$"
