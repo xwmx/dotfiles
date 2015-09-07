1ff3732763ba6d56d121877788659eabc7eb7ab2
William Melody <hi@williammelody.com>
2015-09-06 17:23:28 -0700

    Reformat grep.sh for clarity.

--- home/.shared_env.d/grep.sh
+++ home/.shared_env.d/grep.sh
@@ -3,6 +3,7 @@
 ###############################################################################
 
 # Use color by default
-if echo hello|grep --color=auto l >/dev/null 2>&1; then
+if echo "hello" | grep --color=auto l >/dev/null 2>&1
+then
   export GREP_OPTIONS='--color=auto' GREP_COLOR='1;32'
 fi
