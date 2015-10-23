84fc7782493a1bac4fb2121784eff848a9d84d2c
William Melody <hi@williammelody.com>
2015-10-23 16:02:27 -0700

    Add cmus to brew-programs with OS X workaround in .shared_rc.d.

---
+++ home/.shared_rc.d/cmus.sh
@@ -0,0 +1,19 @@
+###############################################################################
+# cmus
+#
+# cmus is a small, fast and powerful console music player for Unix-like
+# operating systems.
+#
+# https://cmus.github.io
+# https://news.ycombinator.com/item?id=10430512
+###############################################################################
+
+# Silence error messages on El Capitan
+#
+# On El Capitan, a "...deprecated Carbon Component Manager..." error message
+# is displayed when a track is selected to play. More information:
+# https://github.com/cmus/cmus/issues/331
+if is_osx && is_el_capitan
+then
+  alias cmus="cmus 2> /dev/null"
+fi
