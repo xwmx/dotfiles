099339fefa26e7554cdd83be5761a265b6e05870
William Melody <hi@williammelody.com>
2015-11-03 17:45:38 -0800

    Add `finder` shell function.
    `finder` is just an alias for `open -R` on OS X with some additional
    checks. Although `open -R` is not long, it is less obvious and difficult
    to remember.

--- home/.shared_rc.d/osx.sh
+++ home/.shared_rc.d/osx.sh
@@ -40,6 +40,28 @@ clean_ds_store() {
   find . -type f -name '*.DS_Store' -ls -delete
 }
 
+# finder()
+#
+# Usage:
+#   finder <path>
+#
+# Description:
+#   Open the given path in the finder. Alias for `open -R`.
+finder() {
+  local _path="${1:-}"
+  if [[ -z "$_path" ]]
+  then
+    printf "Usage: finder <path>\n"
+    return 1
+  elif [[ ! -e "$_path" ]]
+  then
+    printf "Nothing exists at the given path:\n%s\n" "$_path"
+    return 1
+  else
+    open -R "$_path"
+  fi
+}
+
 # Shell -----------------------------------------------------------------------
 
 # Canonical hex dump; some systems have this symlinked
@@ -50,4 +72,3 @@ command -v md5sum > /dev/null || alias md5sum="md5"
 
 # OS X has no `sha1sum`, so use `shasum` as a fallback
 command -v sha1sum > /dev/null || alias sha1sum="shasum"
-
