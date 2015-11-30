007af4ca5449b3fef0c618b4c469cf88daf7b1f9
William Melody <hi@williammelody.com>
2015-11-30 10:14:47 -0800

    Add ~/.local/bin to $PATH.
    Include minor cleanup of .shared_env.d/paths.sh.

--- home/.shared_env.d/paths.sh
+++ home/.shared_env.d/paths.sh
@@ -1,8 +1,22 @@
 ###############################################################################
-# PATH
+# paths.sh
 #
-# Note: Additional PATH initialization in /etc/paths and /etc/paths.d
+# $PATH settings.
+#
+# Note: Additional $PATH initialization in /etc/paths and /etc/paths.d
 ###############################################################################
 
-# Add homebrew-managed /usr/local/sbin to PATH
-export PATH=/usr/local/sbin:$PATH
+export PATH
+
+# Add homebrew-managed /usr/local/sbin to $PATH
+PATH="/usr/local/sbin:${PATH}"
+
+# Add ~/.local/bin to $PATH
+#
+# `pip install` with the `--user` option installs executables in ~/.local/bin,
+# at least on Ubuntu. ~/.local is part of the XDG Base Directory Specification.
+#
+# More info about ~/.local:
+# http://standards.freedesktop.org/basedir-spec/basedir-spec-latest.html
+# http://askubuntu.com/a/14536
+PATH="${HOME}/.local/bin:${PATH}"
