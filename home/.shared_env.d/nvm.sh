13eb1e6797582f99475634d9d2bb3c85573e37e2
William Melody <hi@williammelody.com>
2015-12-17 17:27:01 -0800

    Reconfigure nodejs and npm so everything is installed with nvm.
    nvm (node version manager) is not compatible with other node
    installations on the same system. Furthermore, in the previous
    configuration all npm packages were installed in the same location,
    which is error prone when switching noder verions.
    
    This commit also resolves at least one issue caused by an nvm
    incompatibility with a symlinked ~/.nvm directory.

--- home/.shared_env.d/nvm.sh
+++ home/.shared_env.d/nvm.sh
@@ -10,7 +10,25 @@
 ###############################################################################
 
 # Node installs will be lost upon upgrading nvm. Add the following above
-# the source line to move install location and prevent this:
-export NVM_DIR=~/.nvm
+# the source line to move install location and prevent this.
+#
+# NOTE: When ~/.nvm is a symlink and `$NVM_DIR` is set to that location,
+# various errors can occur. There is currently an open issue related to this:
+#
+# https://github.com/creationix/nvm/issues/617
+#
+#  One error encountered in this configuration is the following:
+# 'nvm is not compatible with the npm config "prefix" option: currently set to "<path>"'.
+# In this case, the warning appears to be triggered by this check:
+#
+# https://github.com/creationix/nvm/blob/04c5e3540eda71360470c6bd0ef850fefed7190c/nvm.sh#L1356
+#
+# This issue can be resolved by using `readlink`, as noted in this comment:
+#
+# https://github.com/creationix/nvm/issues/855#issuecomment-160232254
+#
+# `readlink` ensures that `$NVM_NPM_PREFIX` will correctly match as a
+# subdirectory of `$NVM_DIR`.
+export NVM_DIR="$(readlink "${HOME}/.nvm")"
 
-. "$HOME"/.nvm/nvm.sh
+. "${HOME}/.nvm/nvm.sh"
