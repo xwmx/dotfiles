112aa618d1a89289dfb8534c6e92443108b94fb4
William Melody <hi@williammelody.com>
2015-12-25 20:43:36 -0800

    Add and configure pyenv with plugins.
    This configuration is a manual, submodule-based version of
    pyenv-installer:
    
    https://github.com/yyuu/pyenv-installer/blob/master/bin/pyenv-installer
    
    It's unknown whether pyenv-installer and pyenv-update will work in this
    configuration, but they are included so this can be determined locally.

---
+++ home/.shared_env.d/pyenv.sh
@@ -0,0 +1,57 @@
+###############################################################################
+# pyenv
+#
+# Simple Python version management
+#
+# pyenv lets you easily switch between multiple versions of Python. It's
+# simple, unobtrusive, and follows the UNIX tradition of single-purpose tools
+# that do one thing well.
+#
+# This project was forked from rbenv and ruby-build, and modified for Python.
+#
+# https://github.com/yyuu/pyenv
+#
+# pyenv plugins:
+# - https://github.com/yyuu/pyenv-doctor
+# - https://github.com/yyuu/pyenv-installer
+# - https://github.com/yyuu/pyenv-update
+# - https://github.com/yyuu/pyenv-virtualenv
+# - https://github.com/yyuu/pyenv-which-ext
+###############################################################################
+
+# _enable_pyenv()
+#
+# Usage:
+#   _enable_pyenv
+#
+# Description:
+#   Initialize pyenv and $PATH for bin directory.
+_enable_pyenv() {
+
+  # Exit if `pyenv` not found.
+  if ! which pyenv > /dev/null
+  then
+    return 0
+  fi
+
+  export PYENV_ROOT="$HOME/.pyenv"
+  export PATH="$PYENV_ROOT/bin:$PATH"
+
+  eval "$(pyenv init -)"
+
+  # Set `$CFLAGS` and `$LDFLAGS`
+  #
+  # Fix for pyenv-doctor build failure with the following message:
+  # 'configure: error: OpenSSL development header is not installed.'
+  #
+  # https://github.com/yyuu/pyenv/wiki/Common-build-problems
+  export CFLAGS
+  export LDFLAGS
+  CFLAGS="${CFLAGS} -I $(brew --prefix openssl)/include"
+  LDFLAGS="${LDFLAGS} -L $(brew --prefix openssl)/lib"
+
+  # https://github.com/yyuu/pyenv-virtualenv
+  eval "$(pyenv virtualenv-init -)"
+}
+
+_enable_pyenv
