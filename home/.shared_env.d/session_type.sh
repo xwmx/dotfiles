bac0e30ef21c96aca5d1f79577e263fd9458b206
William Melody <hi@williammelody.com>
2015-12-31 18:14:26 -0800

    Use newline instead of semicolon in .shared_env.d/session_type.sh.

--- home/.shared_env.d/session_type.sh
+++ home/.shared_env.d/session_type.sh
@@ -6,7 +6,8 @@
 # http://unix.stackexchange.com/a/9607
 ###############################################################################
 
-if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]; then
+if [ -n "$SSH_CLIENT" ] || [ -n "$SSH_TTY" ]
+then
   SESSION_TYPE="remote/ssh"
 else
   case $(ps -o comm= -p $PPID) in
