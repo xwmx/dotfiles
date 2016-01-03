841b66f58144142a808169c4fcf58ade13af1d81
William Melody <hi@williammelody.com>
2016-01-03 10:50:52 -0800

    Add friends to gem install script and configure with .shared_rc.d file.

---
+++ home/.shared_rc.d/friends.sh
@@ -0,0 +1,12 @@
+###############################################################################
+# friends
+#
+# Friends is both a Ruby library and a command-line interface that allows you
+# to keep track of your relationships with the people you care about.
+#
+# https://github.com/JacobEvelyn/friends
+###############################################################################
+
+# Use filename flag to set data file from the default of ./friends.md to
+# ~/.friends.md.
+alias friends="friends --filename '${HOME}/.friends.md'"
