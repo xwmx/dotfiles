4ccb7e33de53194e70ef9656ffbe4814051c4a5e
William Melody <hi@williammelody.com>
2016-01-16 16:51:15 -0800

    Add .shared_env.d/hosts.sh with `alias hosts="sudo hosts"`.

---
+++ home/.shared_rc.d/hosts.sh
@@ -0,0 +1,9 @@
+###############################################################################
+# hosts
+#
+# A command line tool for managing hosts file entries.
+#
+# https://github.com/alphabetum/hosts
+###############################################################################
+
+alias hosts="sudo hosts"
