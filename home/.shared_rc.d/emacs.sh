b20ba74c3e01ae5582b7f538028423e2307bf7b4
William Melody <hi@williammelody.com>
2016-02-02 20:40:09 -0800

    Add `emacsd` shell function for controlling `emacs --daemon`.
    Starting and stopping the emacs daemon apparently requires unintuitive
    commands, so this function encapsulates the operations and includes
    `status` subcommand.
    
    Also included are several aliases to provide consistent shortcuts with
    both the server and client.

---
+++ home/.shared_rc.d/emacs.sh
@@ -0,0 +1,68 @@
+###############################################################################
+# .shared_rc.d/emacs.sh
+#
+# Emacs
+#
+# GNU Emacs text editor
+#
+# https://www.gnu.org/software/emacs/
+###############################################################################
+
+# emacsd
+#
+# Control the emacs daemon.
+emacsd() {
+  if [[ "${1}" =~ '^-h|--help$' ]]
+  then
+    cat <<HEREDOC
+Usage:
+  emacsd (start | status | stop)
+  emacsd -h | --help
+
+Options:
+  -h --help  Display this usage information.
+
+Description:
+  Control the emacs daemon.
+HEREDOC
+  elif [[ "${1}" =~ '^start|up$' ]]
+  then
+    if ! emacsd status &>/dev/null
+    then
+      emacs --daemon &>/dev/null \
+        && printf "\`emacs --daemon\` started.\n"
+    else
+      printf "\`emacs --daemon\` is already running.\n"
+      return 1
+    fi
+  elif [[ "${1}" =~ ^stat ]]
+  then
+    if [[ "$(pgrep -lf emacs)" =~ 'emacs --daemon' ]]
+    then
+      printf "\`emacs --daemon\` is running.\n"
+      return 0
+    else
+      printf "\`emacs --daemon\` is not running.\n"
+      return 1
+    fi
+  elif [[ "${1}" =~ '^stop|down$' ]]
+  then
+    if emacsd status &>/dev/null
+    then
+      # Source, with other approaches:
+      # http://stackoverflow.com/q/1167484
+      emacsclient -e "(kill-emacs)" \
+        && printf "\`emacs --daemon\` stopped.\n"
+    else
+      printf "\`emacs --daemon\` is not running.\n"
+      return 1
+    fi
+  else
+    emacsd -h
+    return 1
+  fi
+}
+
+alias emacsc='emacsclient -t'
+alias emc='emacsclient -t'
+alias emd='emacsd'
