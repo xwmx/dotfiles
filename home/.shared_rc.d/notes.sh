0f100bb8b0d0ac61bd194be23062241dd09331e7
William Melody <hi@williammelody.com>
2016-02-17 20:44:01 -0800

    Add `note` alias for `notes`.

---
+++ home/.shared_rc.d/notes.sh
@@ -0,0 +1,10 @@
+###############################################################################
+# notes
+#
+# A command line note-taking and syncing tool that stores data in Markdown
+# files versioned with git.
+#
+# https://github.com/alphabetum/notes
+###############################################################################
+
+alias note="notes"
