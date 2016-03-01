a92587f1e616e0b97b9b13bc6c72ba7d46c19da1
William Melody <hi@williammelody.com>
2016-02-29 22:00:13 -0800

    Include <notebook>:<subcommand> options in `notes` completions.

--- home/.zshrc.d/notes.autocomplete.zsh
+++ home/.zshrc.d/notes.autocomplete.zsh
@@ -14,10 +14,26 @@
 _notes_subcommands() {
   local _commands
   _commands=($(notes commands))
+  local _notebooks
+  _notebooks=($(notes notebooks --names))
+  local _completions
+  _completions=(${_commands[@]})
+
+  # Construct <notebook>:<subcommand> completions.
+  for __notebook in "${_notebooks[@]}"
+  do
+    for __command in "${_commands[@]}"
+    do
+      if [[ -n "${__notebook:-}" ]] && [[ -n "${__command}" ]]
+      then
+        _completions+=("${__notebook}:${__command}")
+      fi
+    done
+  done
 
   if [[ "${?}" -eq 0 ]]
   then
-    compadd -- "${_commands[@]}"
+    compadd -- "${_completions[@]}"
     return 0
   else
     return 1
