021ab8c4d43addf472ea8cfd9017bf584eb32071
William Melody <hi@williammelody.com>
2016-04-04 21:18:26 -0700

    Quote values in locale.sh config.

--- home/.shared_env.d/locale.sh
+++ home/.shared_env.d/locale.sh
@@ -5,11 +5,11 @@
 # Profiles > Advanced > Set locale environment variables on startup
 ###############################################################################
 
-export LANG=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-export LC_COLLATE=en_US.UTF-8
-export LC_CTYPE=en_US.UTF-8
-export LC_MESSAGES=en_US.UTF-8
-export LC_MONETARY=en_US.UTF-8
-export LC_NUMERIC=en_US.UTF-8
-export LC_TIME=en_US.UTF-8
+export LANG="en_US.UTF-8"
+export LC_ALL="en_US.UTF-8"
+export LC_COLLATE="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+export LC_MESSAGES="en_US.UTF-8"
+export LC_MONETARY="en_US.UTF-8"
+export LC_NUMERIC="en_US.UTF-8"
+export LC_TIME="en_US.UTF-8"
