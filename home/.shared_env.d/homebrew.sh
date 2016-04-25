9464001730af96e7417737173632f8481bfa6765
William Melody <hi@williammelody.com>
2016-04-25 14:15:15 -0700

    Disable homebrew analytics.

---
+++ home/.shared_env.d/homebrew.sh
@@ -0,0 +1,15 @@
+###############################################################################
+# .shared_env.d/homebrew.sh
+#
+# Configuration for Homebrew.
+#
+# http://brew.sh/
+###############################################################################
+
+# Analytics
+#
+# Disable homebrew analytics.
+#
+# https://github.com/Homebrew/brew/blob/master/share/doc/homebrew/Analytics.md
+# https://news.ycombinator.com/item?id=11566720
+export HOMEBREW_NO_ANALYTICS=1
