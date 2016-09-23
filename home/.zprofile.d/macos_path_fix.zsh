c0a41dd17febc76601e8ae5618b6083a324f8f32
William Melody <hi@williammelody.com>
2016-09-23 15:35:57 -0700

    Use `setop global_rcs` step of macos_path_fix in Sierra.

--- home/.zprofile.d/macos_path_fix.zsh
+++ home/.zprofile.d/macos_path_fix.zsh
@@ -4,7 +4,10 @@
 # See "$HOME/.shared_env.d/_macos_path_fix.zsh" for more information.
 ##############################################################################
 
-if is_osx && is_el_capitan
+if is_osx
 then
-  setopt global_rcs
+  if is_el_capitan || is_sierra
+  then
+    setopt global_rcs
+  fi
 fi
