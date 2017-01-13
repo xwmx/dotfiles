7ea833b6077e35aa3589c8a0d0c24a407fcbf9ad
William Melody <hi@williammelody.com>
2017-01-13 14:36:09 -0800

    Override `git` command.
    Require confirmation for `git push production`. Move contents of
    scm_breeze.sh into git.sh to have better control over the execution
    order.

--- home/.shared_rc.d/git.sh
+++ home/.shared_rc.d/git.sh
@@ -10,6 +10,57 @@
 #
 ###############################################################################
 
+# Load scm_breeze.
+#
+# This line would have been added to the rc files by the
+# $HOME/.scm_breeze/install.sh script.
+#
+# Additionally, 1>/dev/null silences the following message:
+#   SCM Breeze must be loaded after RVM...
+#
+# The SCM Breeze installation can be found
+# at $HOME/.scm_breeze
+#
+# https://github.com/ndbroadbent/scm_breeze
+[ -s "$HOME/.scm_breeze/scm_breeze.sh" ] && \
+  source "$HOME/.scm_breeze/scm_breeze.sh" 1>/dev/null
+
+# git()
+#
+# A wrapper function to warn before initiating a `git push` to a production
+# remote.
+export ___SCMB_GIT_FUNCTION
+___SCMB_GIT_FUNCTION=$(which git)
+git() {
+  if [[ "${1:-}" == "push" ]] && [[ "${2:-}" == "production" ]]
+  then
+    while true
+    do
+      printf "🚀 'git push' to 'production'? [y/N] "; read -r yn
+      case ${yn} in
+        [Yy]*)
+          # Run in subshell to avoid global `git()` function override.
+          (
+            eval "${___SCMB_GIT_FUNCTION}"
+            git "${@}"
+          )
+          break
+          ;;
+        *)
+          printf "🛬 'git push' cancelled.\n"
+          break
+          ;;
+      esac
+    done
+  else
+    # Run in subshell to avoid global `git()` function override.
+    (
+      eval "${___SCMB_GIT_FUNCTION}"
+      git "${@}"
+    )
+  fi
+}
+
 # gg is aliased to 'git gui citool' at the following location:
 #
 #   $HOME/.oh-my-zsh/plugins/git/git.plugin.zsh#105
